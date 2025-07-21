6a4a8ba214dac938ac7441a5dab0651953692593
MoizNaeem <moiznaeem20@gmail.com>
2025-07-21 20:55:53 +0300

    Updated the feed to serve only one card

--- frontend/devBuddy/src/components/Feed.jsx
+++ frontend/devBuddy/src/components/Feed.jsx
@@ -5,6 +5,7 @@ import { fetchFeed } from "../../utils/helpers";
 import { useEffect, useState } from "react";
 const Feed = () => {
   const [users, setUsers] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0)
   useEffect(() => {
     (async () => {
       const data = await fetchFeed();
@@ -16,9 +17,12 @@ const Feed = () => {
   return (
     <div className="flex flex-wrap m-10">
         <div className="flex flex-wrap gap-8">
-      {users.map((user) => (
-        <UserCard key={user._id} {...user} />
-      ))}
+      
+        <button className=" bg-black" onClick={()=> {if(currentIndex < users.length) setCurrentIndex(currentIndex-1)}}> Previous </button>
+        <UserCard key={currentIndex} {...users[currentIndex]} />
+        <button className=" bg-black" onClick={()=> setCurrentIndex(currentIndex+1)}> Next </button>
+
+      
     </div>
     </div>
     
