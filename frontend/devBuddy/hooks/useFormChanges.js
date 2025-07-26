cf3bc5ccc81c332c7ff8400f8575705d8bd24791
MoizNaeem <moiznaeem20@gmail.com>
2025-07-26 14:57:23 +0300

    Removed consoles

--- frontend/devBuddy/hooks/useFormChanges.js
+++ frontend/devBuddy/hooks/useFormChanges.js
@@ -5,27 +5,14 @@ export const useFormChanges = (initialValues, currentValues) => {
   const initialRef = useRef((initialValues));
   const [resetCounter, setResetCounter] = useState(0); 
 
-  
-
-  console.log("Initial values: " + initialValues.age)
-
   const hasChanged = useMemo(() => {
-    console.log("inside hasChanged: ")
-
-    console.dir(initialRef.current)
-
-    console.dir(currentValues)
-
-    console.log( isEqual(currentValues, initialRef.current))
 
     return !isEqual(currentValues, initialRef.current);
   }, [currentValues, resetCounter]);
 
   const resetInitial = useCallback((newInitialValues) => {
-    console.log("inside reset: ")
     initialRef.current = newInitialValues;
     setResetCounter(prev => prev + 1);
-    console.log(initialRef.current)
   }, []);
 
   return [hasChanged, resetInitial];
